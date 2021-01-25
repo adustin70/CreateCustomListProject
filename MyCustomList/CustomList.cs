fb45d9692e35a311c965f1ad8f4b53c9b587e48c
Dustin <adustin70@gmail.com>
2021-01-25 16:05:10 -0500

    Removed one datatype method, added methods to test for count, capacity, over capacity and if index remains the same after doubling capacity

--- MyCustomList/CustomList.cs
+++ MyCustomList/CustomList.cs
@@ -8,5 +8,40 @@ namespace MyCustomList
 {
     public class CustomList
     {
+        // member variables
+
+
+        // constructor
+
+
+        // member methods
+        //public void Add()
+        //{
+        //    public void AddItemToCustomList_CheckForCorrectIndex()
+        //    {
+        //        // Arrange
+        //        CustomList<int> list = new CustomList<int>();
+        //        //int[] listArray = new int[2];
+        //        //int counter = 0;
+        //        //int capacity = 4;
+        //        int actual;
+        //        int expected = 30;
+
+        //        // Act
+        //        listArray[0] = 10;
+        //        listArray[1] = 20;
+        //        listArray[2] = 30;
+        //        if (counter == capacity)
+        //        {
+        //            listArray.CopyTo(listArray, counter);
+        //            int[] newListArray = new int[capacity * 2];
+        //        }
+        //        actual = listArray[2];
+
+        //        // Assert
+        //        Assert.AreEqual(expected, actual);
+        //    }
+        //}
+
     }
 }
--- MyCustomListUnitTestProject/AddMethodTest.cs
+++ MyCustomListUnitTestProject/AddMethodTest.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using MyCustomList;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace MyCustomListUnitTestProject
 {
@@ -11,7 +13,7 @@ namespace MyCustomListUnitTestProject
         public void AddItemToCustomList_CheckCountEquals1()
         {
             // Arrange
-            CustomList list = new CustomList();
+            CustomList<string> list = new CustomList<string>();
             string shoe = "Air Jordans";
             int actual;
             int expected = 1;
@@ -26,11 +28,37 @@ namespace MyCustomListUnitTestProject
 
         [TestMethod]
 
+        public void AddItemToCustomList_CheckCountEquals5()
+        {
+            // Arrange
+            CustomList<string> list = new CustomList<string>();
+            string myThoughts1 = "I";
+            string myThoughts2 = "Love";
+            string myThoughts3 = "Learning";
+            string myThoughts4 = "To";
+            string myThoughts5 = "Code";
+            int actual;
+            int expected = 5;
+
+            // Act
+            list.Add(myThoughts1);
+            list.Add(myThoughts2);
+            list.Add(myThoughts3);
+            list.Add(myThoughts4);
+            list.Add(myThoughts5);
+            actual = list.Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
         public void AddItemToCustomList_CheckForItemAtIndex0()
         {
             // Arrange
-            CustomList list = new CustomList();
-            string expected = "Pippin";
+            CustomList<string> list = new CustomList<string>();
+            string expected = "Smith";
             string actual;
 
             // Act
@@ -47,7 +75,7 @@ namespace MyCustomListUnitTestProject
         public void AddItemsToCustomList_CheckForItemAtIndex3()
         {
             // Arrange
-            CustomList list = new CustomList();
+            CustomList<string> list = new CustomList<string>();
             string name1 = "Stephon";
             string name2 = "Marbella";
             string name3 = "JT";
@@ -60,7 +88,6 @@ namespace MyCustomListUnitTestProject
             list.Add(name2);
             list.Add(name3);
             list.Add(name4);
-
             actual = list[3];
 
             // Assert
@@ -68,19 +95,39 @@ namespace MyCustomListUnitTestProject
             Assert.AreEqual(expected, actual);
         }
 
+        
         [TestMethod]
 
-        public void AddItemToCustomList_CheckForItemDataType()
+        public void AddItemToCustomList_CheckForItemValue()
         {
             // Arrange
-            CustomList list = new CustomList();
+           
+            CustomList<int> list = new CustomList<int>();
             int myAge = 31;
             int actual;
-            int expected = myAge;
+            int expected = 31;
 
             // Act
             list.Add(myAge);
-            actual = list.GetType();
+            actual = list[1];
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+        [TestMethod]
+
+        public void AddItemToCustomList_CheckCapacityUponCreation_CapacityShouldBeFour()
+        {
+            // Arrange
+
+            CustomList<int> list = new CustomList<int>();
+            int actual;
+            int expected = 4;
+
+            // Act
+
+            actual = list.Capacity;
 
             // Assert
             Assert.AreEqual(expected, actual);
@@ -88,24 +135,60 @@ namespace MyCustomListUnitTestProject
 
         [TestMethod]
 
-        public void AddItemsToCustomList_CheckForItemDataType2()
+        public void AddItemToCustomList_ExceedingInitialCapacity_CapacityShouldBeDouble()
         {
             // Arrange
-            CustomList list = new CustomList();
-            string bestToJumpFrom = "Black Hawk";
-            int nightJumps = 3;
-            double totalJumps = 25;
-            double actual;
-            double expected = totalJumps;
+
+            CustomList<int> list = new CustomList<int>();
+            int valueOne = 2;
+            int valueTwo = 4;
+            int valueThree = 7;
+            int valueFour = 9;
+            int valueFive = 6;
+            int actual;
+            int expected = 8;
+
+
+
 
             // Act
-            list.Add(bestToJumpFrom);
-            list.Add(nightJumps);
-            list.Add(totalJumps);
-            actual = list.GetType(list[2]);
+            list.Add(valueOne);
+            list.Add(valueTwo);
+            list.Add(valueThree);
+            list.Add(valueFour);
+            list.Add(valueFive);
+            actual = list.Capacity;
+
 
             // Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void AddItemToCustomList_CheckForItemAtIndexTwo_IndexShouldBeSameAsBeforeCopy()
+        {
+            // Arrange
+            CustomList<int> list = new CustomList<int>();
+            int valueOne = 2;
+            int valueTwo = 4;
+            int valueThree = 7;
+            int valueFour = 9;
+            int valueFive = 6;
+            int actual;
+            int expected = valueThree;
+
+            // Act
+            list.Add(valueOne);
+            list.Add(valueTwo);
+            list.Add(valueThree);
+            list.Add(valueFour);
+            list.Add(valueFive);
+            actual = list[2];
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
         }
     }
 }
